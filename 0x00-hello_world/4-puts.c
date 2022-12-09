67c48b2a82cefdafccdec60c715f66cce68141ad
Blessingsundayunaeze <esthersunday620@gmail.com>
2022-12-08 18:39:22 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /*
- * puts: prints a string
- * Return: succes
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	getchar();
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
