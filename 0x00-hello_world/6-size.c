0a449badc3745764487e183120c3f0d1cba30720
Blessingsundayunaeze <esthersunday620@gmail.com>
2022-12-08 19:48:45 -0800

    size od datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int;
+
+	long int;
+
+	float;
+
+	char;
+
+	long long int;
+
+
+	printf("size of int: %zu bytes\n", sizeof(int));
+	printf("size of double: %zu bytes\n", sizeof(long int));
+	printf("size of float: %zu bytes\n", sizeof(float));
+	printf("size of char: %zu bytes\n", sizeof(char));
+	printf("size of char: %zu bytes\n", sizeof(long long int));
+
+	return (0);
+}
