6c3e7018e92349808108ce7e3e5f89d115085674
Blessingsundayunaeze <esthersunday620@gmail.com>
2022-12-13 02:59:04 -0800

    printing putchar with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define MAXSTRING 80
+
+/**
+ * main - printing putchar
+ * Return - Success
+ */
+int main(void)
+{
+	char message[] = "_putchar";
+	int count;
+
+	for (count = 0; count < MAXSTRING; count++)
+	{
+		if (message[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(message[count]);
+	}
+	return (0);
+}
